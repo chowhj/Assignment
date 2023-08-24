aec4730dd2c69e417de1e50a02c755b95bc9a23a
chowhj <hongjunchow@gmail.com>
2023-08-24 22:05:32 +0800

    first edit by Teh Jin En

---
+++ assignment/task/screens/SignoutScreen.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import {View, Text} from "react-native";
+
+export default class SignoutScreen extends Component{
+    render(){
+        return(
+            <View>
+                <Text>
+                    SignoutScreen
+                </Text>
+            </View>
+        )
+    }
+}
