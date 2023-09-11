dbfe013f6d06b35d4991eceba4f44dbee17b72b4
TehJinEn <jinenteh@gmail.com>
2023-09-11 13:01:29 +0800

    changes on the external stylesheet

--- assignment/wireless_task/screens/ProfileScreen.js
+++ assignment/wireless_task/screens/ProfileScreen.js
@@ -1,4 +1,4 @@
-import React, {Component,useEffect} from "react";
+import React, {Component} from "react";
 import {
     Text,
     View,
@@ -12,173 +12,142 @@ import {
     } from "react-native";
 
 import {InputWithLabel} from '../../UI';
-import {FloatingAction} from 'react-native-floating-action';
-import {ScrollView} from 'react-native-gesture-handler';
-import {FlatList} from 'react-native-gesture-handler';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
 import { LogBox } from 'react-native';
+import { externalstyles } from "../style/Externalstylesheet";
 
 LogBox.ignoreLogs([
   'Non-serializable values were found in the navigation state',
 ]);
 
 let SQLite = require('react-native-sqlite-storage');
-
-
+let config = require('../Config')
 
 export default class ProfileScreen extends Component{
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: this.props.route.params.username,
+      email: this.props.route.params.email,
+      signedin: this.props.route.params.signedin,
+      user: null,
+      };
 
-    constructor(props) {
-        super(props);
-        this.state = {
-          name:this.props.route.params?.username,
-          signedin:this.props.route.params?.signedin,
-          user: null,
-          usershow: null,
-          username:'',
-          };
+    this._queryByEmail = this._queryByEmail.bind(this);
 
-      this._queryByName = this._queryByName.bind(this);
-      this._queryShown = this._queryShown.bind(this);
-
-      this.db = SQLite.openDatabase(
-        {name: 'credentialsdb.db', createFromLocation: '~credentialsdb.db'},
-        this.openCallback,
-        this.errorCallback,
-      );
-      }
-
-      componentDidMount() {
-        this._queryByName();
-        this._readSettings();
-        //this._saveSettings(this.state.name)
-        if (this.state.name!=null){
-          this._saveSettings(this.state.name)}
-        else{(this._saveSettings(this.state.username))};
-        //console.log('username',this.state.username);
-        this._queryShown();
-      }
-
-      componentDidUpdate() {
-        this.props.navigation.setOptions({headerTitle: 'Profile  ' + this.state.username });
-      }
-
-      _queryByName() {
-        this.db.transaction(tx =>
-          tx.executeSql(
-            'SELECT * FROM users WHERE username=?',
-            [this.state.name],
-            (tx, results) => {
-              console.log(results.rows.item(0));
-              if (results.rows.length) {
-                this.setState({user: results.rows.item(0)});
-              }
-            },
-          ),
-        );
-      }
-
-      _queryShown(){
-        this.db.transaction(tx =>
-          tx.executeSql(
-            'SELECT * FROM users WHERE username=?',
-            [this.state.username],
-            (tx, results) => {
-              //console.log('Next',results1.rows.item(0));
-              if (results.rows.length) {
-                this.setState({usershow: results.rows.item(0)});
-              }
-            },
-          ),
-        );
-      }
-
-      openCallback() {
-        console.log('Profile Screen Database opened successfully'); 
-      }
-       
-      errorCallback(err) {
-        console.log('Error in opening the database: ' + err);
-      }
-
-      async _saveSettings(uname) {
-        try {
-          await AsyncStorage.setItem('username', uname);
-        } catch (error) {
-          console.log('## ERROR SAVING ITEM ##: ', error);
-        }
-      }
-
-
-      async _readSettings() {
-        try {
-          let username = await AsyncStorage.getItem('username');
-          //console.log('username',username)
-          if (username !== null) {
-            this.setState({username: username});
-          }
-        } catch (error) {
-          console.log('## ERROR READING ITEM ##: ', error);
-        }
-      }
-
-    
-      render() {
-        let user = this.state.usershow;
-          return (
-            <View style={styles.container}>
-                <Image
-                  style={styles.coverPhoto}
-                  source={{uri:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTUUZgqczMg7bPR2O7Sbty2laosPA-WXj24-6Y6dCpMJWI3cRio9PmSJTYAvo3i0ifxjo&usqp=CAU'}}
-                />
-                <View style={styles.profileContainer}>
-                  <Image
-                    style={styles.profilePhoto}
-                    source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCu_zlYqgRAA-Q_oEJX28TY-OoRK2Yi2rlyg&usqp=CAU'}}
-                  />
-                </View>
-                <InputWithLabel
-                  textLabelStyle={styles.TextLabel}
-                  textInputStyle={styles.TextInput}
-                  label={'User Name:'}
-                  value={user ? user.username:''}
-                  orientation={'vertical'}
-                  editable={false}
-                />
-                <InputWithLabel
-                  textLabelStyle={styles.TextLabel}
-                  textInputStyle={styles.TextInput}
-                  label={'User Email:'}
-                  value={user ? user.email:''}
-                  orientation={'vertical'}
-                  editable={false}
-                />
-                <Text></Text>
-                <Text>{'\n'}</Text>
-                <Button
-                      title="Go back Home Screen"
-                      onPress={() => this.props.navigation.goBack()}
-                />
-            </View>
-          );
-      
-      }
+    // this.db = SQLite.openDatabase(
+    //   {name: 'credentialsdb.db', createFromLocation: '~credentialsdb.db'},
+    //   this.openCallback,
+    //   this.errorCallback,
+    // );
     }
 
+  componentDidMount() {
+    this._queryByEmail();
+  }
+
+  componentDidUpdate() {
+    this.props.navigation.setOptions({headerTitle: 'Profile  ' + this.state.user.username});
+  }
+
+  _queryByEmail(){
+    let url = config.settings.serverPath + "/api/users/" + this.state.email;
+    fetch(url)
+        .then(response =>{
+            if (!response.ok){
+                Alert.alert('Error', response.status.toString());
+                throw Error('Error ' + response.status)
+            }
+            return response.json();
+        })
+        .then(user=>{
+          if (user){
+            this.setState({user:user})
+          }
+        }).catch(error => {console.log(error)})
+    }
+  // _queryByEmail() {
+  //   this.db.transaction(tx =>
+  //     tx.executeSql(
+  //       'SELECT * FROM users WHERE email=?',
+  //       [this.state.email],
+  //       (tx, results) => {
+  //         console.log(results.rows.item(0));
+  //         if (results.rows.length) {
+  //           this.setState({user: results.rows.item(0), id: results.rows.item(0).id});
+  //         }
+  //       },
+  //     ),
+  //   );
+  // }
+
+  // openCallback() {
+  //   console.log('Profile Screen Database opened successfully'); 
+  // }
+    
+  // errorCallback(err) {
+  //   console.log('Error in opening the database: ' + err);
+  // }
+
+
+  render() {
+    let user = this.state.user;
+      return (
+        <View style={externalstyles.container}>
+            <Image
+              style={styles.coverPhoto}
+              source={{uri:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTUUZgqczMg7bPR2O7Sbty2laosPA-WXj24-6Y6dCpMJWI3cRio9PmSJTYAvo3i0ifxjo&usqp=CAU'}}
+            />
+            <View style={styles.profileContainer}>
+              <Image
+                style={styles.profilePhoto}
+                source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCu_zlYqgRAA-Q_oEJX28TY-OoRK2Yi2rlyg&usqp=CAU'}}
+              />
+              <TouchableOpacity onPress={() => {this.props.navigation.navigate('EditProfile',{
+                    user: user,
+                    refresh: this._queryByEmail,
+                  })}
+                }
+                >
+                  <View style={styles.aboutbutton}>
+                      <Text style ={styles.introtext}>Edit User Profile</Text>
+                  </View>
+            </TouchableOpacity>
+            </View>
+            <InputWithLabel
+              textLabelStyle={styles.TextLabel}
+              textInputStyle={styles.TextInput}
+              label={'User Name:'}
+              value={user ? user.username:''}
+              orientation={'vertical'}
+              editable={false}
+            />
+            <InputWithLabel
+              textLabelStyle={styles.TextLabel}
+              textInputStyle={styles.TextInput}
+              label={'User Email:'}
+              value={user ? user.email:''}
+              orientation={'vertical'}
+              editable={false}
+            />
+            <InputWithLabel
+                  textLabelStyle={styles.TextLabel}
+                  textInputStyle={styles.TextInput}
+                  label={'User Phone Number:'}
+                  value={(user ? user.phone:'').toString()}
+                  orientation={'vertical'}
+                  editable={false}
+            />
+            <Text>{'\n'}</Text>
+            <Button
+                  title="Go back Home Screen"
+                  onPress={() => this.props.navigation.goBack()}
+            />
+        </View>
+      );
+  }
+}
+
 const styles = StyleSheet.create({
-    container: {
-        flex:1,
-        flexDirection:'column',
-        backgroundColor: '#F0FFFF',
-        borderRadius: 4,
-        borderWidth: 0.5,
-        borderColor: '#d6d7da',
-        alignItems: 'center',
-        justifyContent:'flex-start',
-        padding:1,
-      },
     coverPhoto: {
         width: '100%',
         height: 180,
@@ -206,10 +175,8 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         alignSelf:'center',
       },
-    
     TextInput: {
         color: 'black',
-        //marginRight: 40,
         borderColor: '#ccc',
         borderWidth: 1,
         borderRadius: 5,
@@ -218,6 +185,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         alignSelf:'stretch',
+        textAlign:'center',
       },
     aboutbutton: {
         height:30,
@@ -234,4 +202,3 @@ const styles = StyleSheet.create({
         color: '#046307',
       },
     });
-
